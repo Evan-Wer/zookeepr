80a8f48efd29c4f868452dcfa4feece734c2beeb
Evan-Wer <evan.g.werner@gmail.com>
2021-07-13 17:45:47 -0400

    finished front-end/back-end communication, Zookeepers finished

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -10,106 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Parse incoming JSON data
 app.use(express.json());
-
-
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static('public'));
-
-const { animals } = require('./data/animals');
-
-// Replacing filter function from callback function within app.get  as standalone function
-
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    // Saving the animalsArray as filteredResults here:
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        // Save personalityTraits as a dedicated array.
-        // If personalityTraits is a string, place it into a new array and save.
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        // Loop through each trait in the personalityTraits array:
-        personalityTraitsArray.forEach(trait => {
-            // Check the trait against each animal in the filteredResults array.
-            // Remember, it's initially a copy of the animalsArray.
-            // But here we're updating it for each trait in the .forEach() loop.
-            // For each trait being targeted by the filter, the filteredResults array will contain only the entries that contain that trait.
-            // So at the end we'l have an array of animals that have every one of the trait when the .forEach() loop is finished.
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    // Return the filtered results:
-    return filteredResults;
-}
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-    return animal;
-}
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result){
-        res.json(result);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.post('/api/animals', (req, res) => {
-    // Set id based on what the next index of the aray will be
-    req.body.id = animals.length.toString();
-
-    // Add animal to JSON file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-
-    console.log(animal);
-
-    res.json(animal);
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
